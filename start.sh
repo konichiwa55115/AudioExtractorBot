2fdf0f680ea58a087c89d0663c2cdf4236f49def
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2022-12-28 06:45:17 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,5 +3,6 @@ apt install ffmpeg
 git clone https://github.com/konichiwa55115/AudioExtractorBot /LazyDeveloper
 cd /LazyDeveloper
 pip3 install -r requirements.txt
+apt install ffmpeg
 echo "Starting Bot...."
 python3 -m bot
